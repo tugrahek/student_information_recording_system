fc42a5faf8ab1d18e7122f67a894ca6226356926
Tuğra Hekimoğlu <162835393+tugrahek@users.noreply.github.com>
2026-04-28 14:47:45 +0300

    inital commit

--- Student_information_recording_system.java
+++ Student_information_recording_system.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package student_information_recording_system;
 
-/**
- *
- * @author Kutay
- */
+
 public class Student_information_recording_system {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
